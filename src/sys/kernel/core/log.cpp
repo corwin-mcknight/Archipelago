a1d14ed852db094172d49d24ff486d1704992164
Corwin McKnight <corwin.mcknight@gmail.com>
2024-09-02 19:55:10 -0700

    log: Add "trace" loglevel, ANSI color output Add editor config for editor consistancy

--- src/sys/kernel/core/log.cpp
+++ src/sys/kernel/core/log.cpp
@@ -3,18 +3,48 @@
 #include "kernel/time.h"
 #include "ktl/fixed_string"
 
+// Color codes for the log levels...
+static const char* log_level_colors[] = {
+    "\033[0m",     // Reset
+    "\033[0;37m",  // Debug
+    "\033[0;36m",  // Info
+    "\033[0;33m",  // Warn
+    "\033[0;31m",  // Error
+    "\033[1;91m",  // Fatal
+};
+
 void kernel::system_log::flush() {
-    kernel::time::tick();  // temporary
+    kernel::time::tick();  // TODO: Temporary for testing
     this->last_flushed_sequence =
         this->for_each(this->last_flushed_sequence, [&](const log_message* message) {
             char status = '-';
+            int color = 0;
 
             switch (message->level()) {
-                case log_level::debug: status = 'd'; break;
-                case log_level::info: status = 'i'; break;
-                case log_level::warn: status = 'w'; break;
-                case log_level::error: status = 'E'; break;
-                case log_level::fatal: status = 'F'; break;
+                case log_level::trace:
+                    status = 't';
+                    color = 1;
+                    break;
+                case log_level::debug:
+                    status = 'd';
+                    color = 1;
+                    break;
+                case log_level::info:
+                    status = 'i';
+                    color = 2;
+                    break;
+                case log_level::warn:
+                    status = 'w';
+                    color = 3;
+                    break;
+                case log_level::error:
+                    status = 'E';
+                    color = 4;
+                    break;
+                case log_level::fatal:
+                    status = 'F';
+                    color = 5;
+                    break;
             }
 
             time_ns_t timestamp = kernel::time::ktime_to_ns(message->timestamp);
@@ -22,20 +52,22 @@ void kernel::system_log::flush() {
             uint64_t time_ms = (uint64_t)(timestamp / 1'000'000) % 1'000;
 
             // Render the front string
-            ktl::fixed_string<16> front;
-            ktl::format::format_to_buffer_raw(front.m_buffer, 50, "{0:03d}.{1:03d} {2:1c} | ", time_seconds,
-                                              time_ms, status);
+            ktl::fixed_string<32> front;
+            ktl::format::format_to_buffer_raw(front.m_buffer, front.length(),
+                                              "{0:s}{1:03d}.{2:03d} {3:1c} | ", log_level_colors[color],
+                                              time_seconds, time_ms, status);
 
             for (auto dev : devices) {
                 dev->write_string(front);
                 message->text.for_each([&](char c) {
                     dev->write_byte(c);
                     if (c == '\n') {
-                        for (size_t i = 0; i < front.length() - 2; i++) { dev->write_byte(' '); }
+                        for (size_t i = 0; i < 10; i++) { dev->write_byte(' '); }
                         dev->write_byte('|');
                         dev->write_byte(' ');
                     }
                 });
+                dev->write_string("\033[0m");
                 dev->write_byte('\n');
             }
         });
