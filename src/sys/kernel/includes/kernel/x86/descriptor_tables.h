73b50447f8858bebdc3eef73646ebfa77f63de51
Corwin McKnight <corwin.mcknight@gmail.com>
2024-09-02 19:56:11 -0700

    x86: Add basic support for PIC, ISR, IRQ

--- src/sys/kernel/includes/kernel/x86/descriptor_tables.h
+++ src/sys/kernel/includes/kernel/x86/descriptor_tables.h
@@ -4,6 +4,30 @@
 
 namespace kernel {
 namespace x86 {
+
+constexpr uint8_t IDT_ENTRY_ATTR_PRESENT = 0x80;
+constexpr uint8_t IDT_ENTRY_ATTR_DPL0 = 0x00;
+constexpr uint8_t IDT_ENTRY_ATTR_DPL3 = 0x60;
+constexpr uint8_t IDT_ENTRY_ATTR_INTERRUPT = 0x05;
+
+// IRQ
+constexpr uint8_t IRQ0 = 32;
+constexpr uint8_t IRQ1 = 33;
+constexpr uint8_t IRQ2 = 34;
+constexpr uint8_t IRQ3 = 35;
+constexpr uint8_t IRQ4 = 36;
+constexpr uint8_t IRQ5 = 37;
+constexpr uint8_t IRQ6 = 38;
+constexpr uint8_t IRQ7 = 39;
+constexpr uint8_t IRQ8 = 40;
+constexpr uint8_t IRQ9 = 41;
+constexpr uint8_t IRQ10 = 42;
+constexpr uint8_t IRQ11 = 43;
+constexpr uint8_t IRQ12 = 44;
+constexpr uint8_t IRQ13 = 45;
+constexpr uint8_t IRQ14 = 46;
+constexpr uint8_t IRQ15 = 47;
+
 struct gdt_entry {
     uint16_t limit_low;
     uint16_t base_low;
@@ -12,6 +36,7 @@ struct gdt_entry {
     uint8_t granularity;
     uint8_t base_high;
 } __attribute__((packed));
+
 struct tss_entry {
     uint32_t unused0;
     uint64_t rsp[3];
@@ -39,7 +64,27 @@ struct gdt {
     struct tss_entry tss;
 } __attribute__((packed));
 
+struct idt_entry {
+    uint16_t base_lo;
+    uint16_t selector;
+    uint8_t ist;
+    uint8_t flags;
+    uint16_t base_mid;
+    uint32_t base_hi;
+    uint32_t _reserved;
+} __attribute__((__packed__));
+
+struct idt_ptr {
+    uint16_t limit;
+    uintptr_t base;
+} __attribute__((__packed__));
+
 void init_gdt(int corenum);
+void init_idt();
+
+void idt_set_gate(unsigned char num, uintptr_t base, unsigned short sel, unsigned char flags);
+void enable_interrupts();
+void disable_interrupts();
 
 }  // namespace x86
 };  // namespace kernel
--- src/sys/kernel/x86_64/main.cpp
+++ src/sys/kernel/x86_64/main.cpp
@@ -7,56 +7,66 @@
 #include <ktl/result>
 #include <ktl/static_array>
 
+#include "kernel/assert.h"
 #include "kernel/config.h"
 #include "kernel/drivers/uart.h"
 #include "kernel/log.h"
 #include "kernel/panic.h"
 #include "kernel/x86/descriptor_tables.h"
+#include "vendor/limine.h"
 
 extern "C" void init_global_constructors_array(void);
 
 kernel::system_log g_log;
 kernel::driver::uart uart;
 
+static volatile struct limine_rsdp_request rsdp_request = {.id = LIMINE_RSDP_REQUEST, .revision = 0};
+
 void init_log() {
     uart.init();
     g_log.devices.push_back(&uart);
 }
 
+void init_acpi() {
+    // Print RDSP info
+    if (rsdp_request.response == NULL) { g_log.error("No RSDP found, halting"); }
+
+    auto rsdp = rsdp_request.response;
+    g_log.info("RSDP: 0x{0:p}", (uint64_t)rsdp->address);
+}
+
+void init_core_tables(int core_id) {
+    kernel::x86::init_gdt(core_id);
+    kernel::x86::init_idt();
+    kernel::x86::enable_interrupts();
+    g_log.debug("cpu{0}: Interrupts Enabled", core_id);
+}
 void core_init(int core_id) {
     // Start basic hardware initialisation
-    g_log.debug("<core{0}> state=init", core_id);
-    kernel::x86::init_gdt(core_id);
-    // kernel::x86::init_idt(core_id);
+    g_log.debug("cpu{0}: Initializing", core_id);
+    init_core_tables(core_id);
 
     // Boot Processor is responsible for initializing the time
     if (core_id == 0) {
         kernel::time::init(104500000);  // TODO: Replace Dummy Number with real timer resolution
         g_log.info("Time subsystem initialized", core_id);
+
+        init_acpi();
     }
 
-    g_log.debug("<core{0}> state=running", core_id);
-}
+    assert(core_id == 0, "Does not support booting other cores");
 
-void bp_init() {
-    init_log();
-    // Display version message
-    g_log.info("Starting Archipelago ver. {0}", CONFIG_KERNEL_VERSION);
-
-    // Init timing
-
-    core_init(0);
+    g_log.debug("cpu{0}: Now running", core_id);
 }
 
 extern "C" void _start(void) {
-    // Initialise runtime
+    // Boot processor specific stuff...
     init_global_constructors_array();
-    bp_init();
+    init_log();
+    g_log.info("Starting Archipelago ver. {0}", CONFIG_KERNEL_VERSION);
+    core_init(0);
 
     // Kernel failed to start.
-    g_log.fatal(
-        "The system was unable to boot successfully, and will now halt completely.\n"
-        "The system will be entirely unresponsive, and will need to be restarted.");
     panic("Boot processor exited early");
     // Processor is halted.
 }
