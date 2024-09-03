eb47111a9e26172da5ce2e0c503a0b296e518bc0
Corwin McKnight <corwin.mcknight@gmail.com>
2024-09-02 19:57:44 -0700

    kernel: Update assertions

--- src/sys/kernel/includes/kernel/assert.h
+++ src/sys/kernel/includes/kernel/assert.h
@@ -9,20 +9,20 @@ constexpr bool assertions_enabled = true;
 enum class assertion_action { panic, warn, debug };
 
 template <typename T, assertion_action A = assertion_action::panic>
-void kernel_assert(T condition, const char* message, const char* fname, int line,
+void kernel_assert(T condition, const char* message, const char* message_text, const char* fname, int line,
                    assertion_action action = A) {
     if constexpr (assertions_enabled) {
         if (!condition) {
             switch (action) {
                 default:
                 case assertion_action::debug:
-                    g_log.debug("Assertion failed: {0}, {1}:{2}", message, fname, line);
+                    g_log.debug("Assertion failed: {0} ({1}), {2}:{3}", message_text, message, fname, line);
                     break;
                 case assertion_action::warn:
-                    g_log.warn("Assertion failed: {0}, {1}:{2}", message, fname, line);
+                    g_log.warn("Assertion failed: {0} (1}), {2}:{3}", message_text, message, fname, line);
                     break;
                 case assertion_action::panic:
-                    g_log.fatal("Assertion failed: {0}, {1}:{2}", message, fname, line);
+                    g_log.fatal("Assertion failed: {0} ({1}), {2}:{3}", message_text, message, fname, line);
                     panic("Assertion failed");
                     break;
             }
@@ -31,12 +31,10 @@ void kernel_assert(T condition, const char* message, const char* fname, int line
 }
 
 #undef assert
-#define assert(x, msg) kernel_assert(x, msg, __FILE__, __LINE__)
-#define assert_and(x, msg, y) kernel_assert(x, msg, __FILE__, __LINE__, y)
+#define assert(x, msg) kernel_assert(x, msg, #x, __FILE__, __LINE__)
+#define assert_and(x, msg, y) kernel_assert(x, msg, #x, __FILE__, __LINE__, y)
 
-#endif
-
-// Assertions I'd like to implement:
+// TODO: Assertions I'd like to implement:
 // - assert
 // - assert_eq
 // - assert_ne
@@ -47,3 +45,5 @@ void kernel_assert(T condition, const char* message, const char* fname, int line
 // - assert_null
 // - assert_not_null
 // - assert_warn
+
+#endif
