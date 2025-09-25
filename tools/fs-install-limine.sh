7a00690194e018eb1f89f57d1bb35dcfa6aa4318
Corwin McKnight <corwin.mcknight@gmail.com>
2025-09-25 07:52:42 +0000

    kernel: Upgrade limine to 10.x

--- tools/fs-install-limine.sh
+++ tools/fs-install-limine.sh
@@ -5,27 +5,27 @@ TOOLS_DIR=$(pwd)
 mkdir -p ../build/tools
 cd ../build/tools
 if [ ! -d "limine" ]; then   
-    git clone https://github.com/limine-bootloader/limine.git --branch=v4.x-branch-binary --depth=1
+    git clone https://github.com/limine-bootloader/limine.git --branch=v10.x-binary --depth=1
     make -C limine
 fi
 
 cd ..
 
 mkdir -p sysroot
-cd tools/limine
-cp limine.sys limine-cd.bin limine-cd-efi.bin ../../sysroot/
-cd ../..
-
 cp -r ../media/* ./sysroot/
 
-xorriso -as mkisofs -b limine-cd.bin \
+cd tools/limine
+cp limine-bios-cd.bin limine-uefi-cd.bin limine-bios.sys ../../sysroot/boot/
+cd ../..
+
+xorriso -as mkisofs -b boot/limine-bios-cd.bin \
         -no-emul-boot -boot-load-size 4 -boot-info-table \
-        --efi-boot limine-cd-efi.bin \
+        --efi-boot boot/limine-uefi-cd.bin \
         -efi-boot-part --efi-boot-image --protective-msdos-label \
         --quiet \
         sysroot -o image.iso
 
-./tools/limine/limine-deploy --quiet image.iso
+./tools/limine/limine bios-install image.iso
 
 
 cd $TOOLS_DIR
